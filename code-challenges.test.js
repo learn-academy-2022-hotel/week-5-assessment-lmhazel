e26eaba949feac631d19c35e0687b31c7ab7ddb1
lmhazel <leahazel2000@gmail.com>
2023-01-17 23:50:01 -0800

    attempted week 5 assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -12,6 +12,12 @@
 
 // --------------------1) Create a function that takes in a string and returns a string with a coded message. The coded message converts "a" to 4, "e" to 3, "i" to 1, and "o" to 0.
 
+//Pseudo Code:
+//Input: a string
+//Output: the characters in the string are changed as such:a" to 4, "e" to 3, "i" to 1, and "o" to 0.
+//Function: The function specialWord changes the input into an array with each character being a different value in the array. The function codeMaker uses the map funtion to iterate throughout the values in specialWord and invokes a conditional function called codeFunction which uses conditions to see if the value is equal to a character that is supposed to change to a different character and if it is, then it returns that value as the character. If not, then it returns the original value. Then the code returns the new array with the join method with quotation marks as the parameter in order for it to return as a string and not an array.
+
+
 // a) Create a test with expect statements using the variables provided.
 
 const secretCodeWord1 = "Lackadaisical"
@@ -21,9 +27,40 @@ const secretCodeWord2 = "Gobbledygook"
 const secretCodeWord3 = "Eccentric"
 // Expected output: "3cc3ntr1c"
 
+describe("codeMaker", () => {
+    it("returns a string but with the appropriate characters changed to different characters", () => {
+        expect(codeMaker()).toEqual("L4ck4d41s1c4l")
+    })
+})
 // b) Create the function that makes the test pass.
 
+specialWord = secretCodeWord1.split('')
+
+const codeFunction = (value) =>{ 
+        if (value === "a"){
+            return "4"
+        } else if (value === "e"){
+            return "3"
+        } else if (value === "i"){
+            return "1"
+        } else if (value === "o"){
+            return "0"
+        } else {
+            return value
+        }
+    }
+
+const codeMaker = (word) => {
+    return word.map(codeFunction)
+}
+codeMaker(specialWord).join("")
+
 // --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
+
+//Pseudo Code:
+//Input: An array of words and a single letter
+//Output: An array of all words containing that particular letter
+//Function: The function fruitOrganizer takes a letter in which it first sets the variable capLetter which is a capitalized version of the letter. Then the function uses the filter method and returns only the words that include the input or capLetter. 
 
 // a) Create a test with expects statement using the variable provided.
 
@@ -34,11 +71,35 @@ const letterA = "a"
 const letterE = "e"
 // Expected output: ["Cherry", "Blueberry", "Peach"]
 
-// b) Create the function that makes the test pass.
+describe("fruitOrganizer", () => {
+        it("returns an array of all the words containing a particular letter", () => {
+         expect(fruitOrganizer(letterA)).toEqual(["Mango", "Apricot", "Peach"])
+         expect(fruitOrganizer(letterE)).toEqual(["Cherry", "Blueberry", "Peach"])
+        })
+    })
+
+// // b) Create the function that makes the test pass.
+const fruitOrganizer = (letter) => {
+    let fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+    let capLetter = letter.toUpperCase()
+    return fruitArray.filter((value) => {
+        if (value.includes(letter)){
+            return value
+        } else if (value.includes(capLetter)){
+            return value
+        }
+    })
+}
+fruitOrganizer(letterA)
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.
 
 // a) Create a test with expect statements using the variable provided.
+
+//Pseudo Code:
+//Input: an array of 5 numbers
+//Output: true or false
+//Function: The function called fullHouse which assesses if the index includes a pair using a for loop function called onePair. This function puts the array in ascending order in order to iterate through the array and see if at the index, the variable right after matches the variable that the function is currently assessing. If it does match then that would be a pair and the function would add an increment to the variable tallyPairs. At the end, the function would see how many pairs are found via tallyPairs. However, I'm not sure how to not account for the three of a kind variables in an array nor do I understand how to create a function that would count three of a kind. 
 
 const hand1 = [5, 5, 5, 3, 3]
 // Expected output: true
@@ -49,4 +110,26 @@ const hand3 = [5, 5, 5, 5, 4]
 const hand4 = [7, 2, 7, 2, 7]
 // Expected output: true
 
+describe("fullHouse", () => {
+    it("returns true or false based on if the array is a full house", () => {
+        expect(fullHouse(hand1)).toEqual("true")
+        expect(fullHouse(hand2)).toEqual("true")
+        expect(fullHouse(hand3)).toEqual("false")
+        expect(fullHouse(hand4)).toEqual("true")
+    })
+})
+
 // b) Create the function that makes the test pass.
+
+const onePair = (array) => {
+    let sortedArray = array.sort((a, b) => a - b)
+    let tallyPairs = 0
+    for(let i = 0; i < array.length; i++){
+        if ([i] === [i + 1]) {
+            tallyPairs + 1
+        } else {tallyPairs + 0}
+    }
+    console.log(tallyPairs)
+}
+onePair(hand1)
+
--- interview-questions.md
+++ interview-questions.md
@@ -8,44 +8,44 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What does calling super do in a class?
 
-Your answer:
+Your answer: Calling super in a class calls down information to be passed to a subclass
 
-Researched answer:
+Researched answer: Super() is a keyword that initializes superclass can pass down information to a subclass. This process is called inheritance. The class that is passing down the information is called a parentclass while the class that is receiving the information is called a childclass. This is useful when you want to create a class that includes information from another class.
 
 2. What is a gem?
 
-Your answer:
+Your answer: In Ruby, a gem is similar to a method in JavaScript and is existing code that can be invoekd and used without having to create your own code. 
 
-Researched answer:
+Researched answer: In Ruby, a gem is similar to a method in JavaScript and is existing code that can be invoekd and used without having to create your own code. This is useful for commonly used methods to cut down on lines of code and save time while coding. 
 
 3. What is a relational database? Are there other kinds of databases?
 
-Your answer:
+Your answer: Relational databases that can be used to go through the data in unique ways to analyze different parts of it. 
 
-Researched answer:
+Researched answer: A relational database is a database in which there are tables that have columns and rows containing information. These columns and rows create relationships among the informaiton. 
 
 4. What are primary keys? Why are they important?
 
-Your answer:
+Your answer:The primary key is the number used to track different data in a table. They are important in order to distinguish and call upon the different parts of data in tables. 
 
-Researched answer:
+Researched answer:The primary key is an ID that is assigned to different data in a table. They are important in order to cross-referance the data in other tables. These are automatically added by rails and shows how useful rails can be.
 
 5. What are the HTTP verbs? What is each verb's corresponding CRUD action?
 
-Your answer:
+Your answer: HTTP verbs are methods in which an action can be completed. The verb's corresponding CRUD action are create, read, update, delete.
 
-Researched answer:
+Researched answer: The HTTP verbs are a set of methods that can be performed. They tell what a server to do based on the methods invoked. The verb's corresponding CRUD action are create, read, update, delete.
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. RESTful routes:
+1. RESTful routes: the standard for mapping among HTTP verbs, controller actions, and CRUD operations
 
-2. Model validations:
+2. Model validations: checking if model outputs are accurate compared to real observatins 
 
-3. Params:
+3. Params: these are URL parameters in which the user can pass in information to change the viewing of data
 
-4. ERB:
+4. ERB:(Embedded Ruby) used to embed ruby into a text file
 
-5. API:
+5. API:(Application Programming Interface) is information passed in the form of JSON which is JavaScript Object Notation. 
